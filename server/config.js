e39908bf80e298c2421fd15ace0fe85ef28f8f0e
JasonJin0923 <903054278@qq.com>
2016-11-01 21:30:00 +0800

    token

---
+++ server/config.js
@@ -0,0 +1,15 @@
+/**
+ * 微信认证后的 app 信息
+ * 如果有需要可以将这些信息放到数据库中读取方便以更增加
+ * 注：测试时请替换您真实的数据
+ */
+module.exports = function(){
+	return {
+		//AppID(应用ID)
+		appid: 'wxe90e7bd70aac5d5d',
+		//AppSecret(应用密钥)
+		appsecret: '3f85ee21f96d0cc86b87a39d8a6ba10e',
+		//Token(令牌)
+		token: 'jinyunxiang'
+	};
+};
